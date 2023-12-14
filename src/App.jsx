46c76c74f80f56b67377fd41d5097c9bf64593bc
h662 <h662hong@gmail.com>
2023-12-14 12:35:53 +0900

    Card Rotate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 const App = () => {
-  return <div className="bg-red-100">App</div>;
+  const [xDeg, setXDeg] = useState();
+  const [yDeg, setYDeg] = useState();
+
+  const onMouseMoveImage = (e) => {
+    setXDeg(e.clientY * (-1 / 5) + 20);
+    setYDeg(e.clientX * (1 / 5) - 20);
+  };
+
+  return (
+    <div className="bg-red-100 min-h-full">
+      <img
+        onMouseMove={onMouseMoveImage}
+        style={{
+          transform: `perspective(200px) rotateX(${xDeg}deg) rotateY(${yDeg}deg)`,
+        }}
+        src={`${process.env.PUBLIC_URL}/images/owl.png`}
+        alt="owl"
+      />
+    </div>
+  );
 };
 
 export default App;
